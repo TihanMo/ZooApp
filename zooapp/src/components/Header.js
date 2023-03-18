b40aa184b388514fd1097d1eb93c11f85500dd47
Giaciii <giacun.masueger@stud.kbw.ch>
2023-03-18 17:26:21 +0100

    Menu Stiche hinzugefügt

--- zooapp/src/components/Header.js
+++ zooapp/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../App.css';
+import bild from '../sys_bilder/menu_strich.png';
 
 export default function Header() {
   return (
     <div>
-      
+      <img src={bild} alt="SVG as an image" className='menu_strich'></img>
       <div className='trenn_strich'></div>
     </div>
   )
