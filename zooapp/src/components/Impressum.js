6d053f1debf1b839a692fcd77d3b59dfa3bae7d5
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-03-18 16:49:29 +0100

    added title 2

--- zooapp/src/components/Impressum.js
+++ zooapp/src/components/Impressum.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function impressum() {
   return (
     <div>
-      <h1>
-        Impressum Test Title
-      </h1>
+        <h1>Impressum Title</h1>
+        <h1>title2</h1>
     </div>
   )
 }
