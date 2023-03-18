2926eaa131e337bc545879c51123773e3f9790c5
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-03-18 16:33:33 +0100

    general component changes

--- zooapp/src/App.js
+++ zooapp/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Homer from './components/Homer';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Homer/>
+      <Footer/>
     </div>
   );
 }
--- zooapp/src/components/Homer.js
+++ zooapp/src/components/Homer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './App.css';
 
 export default function test_component() {
   return (
     <div>
-      
+      <h1>HOME</h1>
     </div>
   )
 }
