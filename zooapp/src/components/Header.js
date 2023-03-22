bdd3b0837d81255dad225efcbb348eda87b0ec8f
Giaciii <giacun.masueger@stud.kbw.ch>
2023-03-22 15:20:57 +0100

    CSS von Header und Logo
    Katastrophe

--- zooapp/src/components/Header.js
+++ zooapp/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../App.css';
 import bild from '../sys_bilder/menu_strich.png';
+import logo from '../logos/png/logo-black.png';
 
 export default function Header() {
   return (
     <div className='header_top'>
       <img src={bild} alt="Menü Logo" className='menu_strich header_inhalt'></img>
+      <img src={logo} alt='Logo' className='header_inhalt menu_strich' id='logo'></img>
       <button className='header_inhalt'>Anmelden</button>
-      <div className='trenn_strich'></div>
     </div>
   )
 }
