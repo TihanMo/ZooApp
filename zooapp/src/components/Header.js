46d13e5e515e6c9764f2ccec95b11fc071359d59
Giaciii <giacun.masueger@stud.kbw.ch>
2023-03-22 14:01:38 +0100

    Weiteres Update für Header

--- zooapp/src/components/Header.js
+++ zooapp/src/components/Header.js
@@ -5,7 +5,7 @@ import bild from '../sys_bilder/menu_strich.png';
 export default function Header() {
   return (
     <div className='header_top'>
-      <img src={bild} alt="Menü Logo" className='menu_strich'></img>
+      <img src={bild} alt="Menü Logo" className='menu_strich header_inhalt'></img>
       <button className='header_inhalt'>Anmelden</button>
       <div className='trenn_strich'></div>
     </div>
