a5b53b1f4d92c50da7edf2f69a1836bbe2935085
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-03-25 17:44:58 +0100

    home bg image added

--- zooapp/src/components/Home.js
+++ zooapp/src/components/Home.js
@@ -5,24 +5,26 @@ import TextBox from './TextBox';
 export default function Home() {
   return (
     <div>
-      <TextBox
-        classname="TextBox"
-        name="Unsere Tiere"
-        description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
-         den Bengaltiger, den eurasischen Wolf, den asiatischen Elefanten und viele mehr!"
-      />
+      <div id="image_bg">
+        <TextBox
+          classname="TextBox"
+          name="Unsere Tiere"
+          description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
+          den Bengaltiger, den eurasischen Wolf, den asiatischen Elefanten und viele mehr!"
+        />
 
-      <TextBox
-        classname="TextBox"
-        name="Wo sind wir?"
-        description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
-      />
+        <TextBox
+          classname="TextBox"
+          name="Wo sind wir?"
+          description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
+        />
 
-      <TextBox
-        classname="TextBox"
-        name="Öffnungszeiten"
-        description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
-      />
+        <TextBox
+          classname="TextBox"
+          name="Öffnungszeiten"
+          description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
+        />
+      </div>
     </div>
   )
 }
