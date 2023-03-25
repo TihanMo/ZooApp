50afaa513901b0812937b3253f7367c8091d1503
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-03-25 17:26:32 +0100

    TextBox Home added

--- zooapp/src/App.js
+++ zooapp/src/App.js
@@ -8,8 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
-      <Impressum />
+      <Home />
       <Footer/>
     </div>
   );
--- zooapp/src/components/Home.js
+++ zooapp/src/components/Home.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import '../App.css';
+import TextBox from './TextBox';
 
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <TextBox
+        classname="TextBox"
+        name="Unsere Tiere"
+        description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
+         den Bengaltiger, den eurasischen Wolf, den asiatischen Elefanten und viele mehr!"
+      />
+
+      <TextBox
+        classname="TextBox"
+        name="Wo sind wir?"
+        description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
+      />
+
+      <TextBox
+        classname="TextBox"
+        name="Öffnungszeiten"
+        description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
+      />
     </div>
   )
 }
