81661d512c0448f0b4877d6e82583eba2005e1d4
Giaciii <giacun.masueger@stud.kbw.ch>
2023-03-25 17:35:41 +0100

    ids und classes update

--- zooapp/src/components/Footer.js
+++ zooapp/src/components/Footer.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <div className='footer'>
-      <div className='header_inhalt' id='menu_logo'>
+      <div className='footer_inhalt' id='menu_logo'>
         <Link to="impressum">Impressum</Link>
       </div>
-      <div className='header_inhalt' id='logo'>
+      <div className='footer_inhalt' id='logo_text'>
         <Link to="/">ZooZApp</Link>
       </div>
-      <div className='header_inhalt' id='btn_anmelden'>
-        <Link to="/">Hilfe</Link>
+      <div className='footer_inhalt' id='hilfe'>
+        <Link to="/hilfe">Hilfe</Link>
       </div>
     </div>
   )
