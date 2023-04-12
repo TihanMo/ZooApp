942b67642bf62bc8b8c38a32ca5cedbc54af8a6e
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-04-12 14:38:23 +0200

    Karte CSS updated

--- zooapp/src/components/Karte.js
+++ zooapp/src/components/Karte.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Karte() {
   return (
-    <div>
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1jtvzzw2JuiUekudrsQIg9UwnFnE3eGs&ehbc=2E312F" width="640" height="480"></iframe>
+    <div id='karte'>
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1jtvzzw2JuiUekudrsQIg9UwnFnE3eGs&ehbc=2E312F" width="1200" height="600"></iframe>
     </div>
   )
 }
