d5d44d6d2d175ea7a85d4c61f5cbebbf21e513d4
Giaciii <giacun.masueger@stud.kbw.ch>
2023-04-19 10:52:13 +0200

    Homesite angepasst

--- zooapp/src/components/Home.js
+++ zooapp/src/components/Home.js
@@ -4,27 +4,25 @@ import TextBox from './TextBox';
 
 export default function Home() {
   return (
-    <div>
-      <div id="image_bg">
-        <TextBox
-          id="tb_1"
-          name="Unsere Tiere"
-          description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
+    <div id="image_bg">
+      <TextBox
+        id="tb_1"
+        name="Unsere Tiere"
+        description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
           den Bengaltiger, den eurasischen Wolf, den asiatischen Elefanten und viele mehr!"
-        />
+      />
 
-        <TextBox
-          id="tb_2"
-          name="Wo sind wir?"
-          description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
-        />
+      <TextBox
+        id="tb_2"
+        name="Wo sind wir?"
+        description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
+      />
 
-        <TextBox
-          id="tb_3"
-          name="Öffnungszeiten"
-          description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
-        />
-      </div>
+      <TextBox
+        id="tb_3"
+        name="Öffnungszeiten"
+        description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
+      />
     </div>
   )
 }
