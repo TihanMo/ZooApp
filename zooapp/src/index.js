a2218130ddab7c00b25a7235728e0f0fad8cfd96
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-04-19 10:38:37 +0200

    Hilfe seite verlinkt und css repariert

--- zooapp/src/index.js
+++ zooapp/src/index.js
@@ -14,13 +14,18 @@ import Login from './components/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="impressum" element={<Impressum />} />
-          <Route path="datenschutz" element={<Datenschutz />} />
-        </Route>
-      </Routes>
+    <GoogleOAuthProvider clientId="809125277994-6r99cua968kh7apnmic8vasle3k545kv.apps.googleusercontent.com">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="impressum" element={<Impressum />} />
+            <Route path="datenschutz" element={<Datenschutz />} />
+            <Route path="login" element={<Login />} />
+            <Route path="karte" element={<Karte/>} />
+            <Route path="hilfe" element={<Hilfe />} />
+          </Route>
+        </Routes>
+      </GoogleOAuthProvider>
     </BrowserRouter>
 );
 
