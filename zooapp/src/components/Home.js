2a6ebafa2f218d629da54f451ec0a90733cdb51d
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-05-17 12:55:23 +0200

    fehler korrigiert

--- zooapp/src/components/Home.js
+++ zooapp/src/components/Home.js
@@ -5,26 +5,28 @@ import TextBox from './TextBox';
 export default function Home() {
   return (
     <div id="image_bg">
-
+    <div id="boxhome1">
       <TextBox
         id="tb_1"
         name="Unsere Tiere"
         description="Wir bieten viele besondere Tiere für unsere Gäste an wie z.B.
           den Bengaltiger, den eurasischen Wolf, den asiatischen Elefanten und viele mehr!"
       />
-
+    </div>
+    <div id="boxhome2">
       <TextBox
         id="tb_2"
         name="Wo sind wir?"
         description="Unser Zoo befindet sich auf der Arcosstrasse 1, Zürich. Jeder ist herzlich Wilkommen!"
       />
-
+    </div>
+    <div id="boxhome3">
       <TextBox
         id="tb_3"
         name="Öffnungszeiten"
         description="Wir haben Montag bis Freitag von 8:00-21:00 offen und an Samstagen 9:00-18:00"
       />
-
+    </div>
     </div>
   )
 }
